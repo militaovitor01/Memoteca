0b9744c5b88f8862016ce065cbbd8fbf9ff5b9c3
militaovitor01 <vdiasmilitao@gmail.com>
2025-02-03 18:08:00 -0300

    Add Create Memorias

--- js/api.js
+++ js/api.js
@@ -7,6 +7,22 @@ const api = {
             alert ('Erro ao buscar pensamentos')
             throw error
         }
+    },
+
+    async cadastrarPensamentos(pensamento){
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)                       
+            })
+            return await response.json()
+        } catch (error) {
+            alert ('Erro ao buscar pensamentos')
+            throw error
+        }
     }
 }
 
--- js/ui.js
+++ js/ui.js
@@ -1,24 +1,43 @@
 import api from "./api.js"
+const listaPensamentos = document.getElementById('lista-pensamentos')
 
 const ui = {
     async renderizarPensamentos() {
-        const listaPensamentos = document.getElementById('lista-pensamentos')
-
         try {
             const pensamentos = await api.buscarPensamentos()
-            pensamentos.forEach(pensamento => {
-                listaPensamentos.innerHTML += `
-                    <li class="li-pensamento" data-id="${pensamento.id}">
-                    <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
-                    <div class="pensamento-conteudo">${pensamento.conteudo}</div>
-                    <div class="pensamento-autoria">${pensamento.autoria}</div>
-                    </li>
-                `
-            });
+            pensamentos.forEach(ui.cadastrarPensamentosNaLista)
         } catch (error) {
             alert('Erro ao renderizar pensamentos')
         }
-    }
+    },
+
+    cadastrarPensamentosNaLista(pensamento) {
+        const li = document.createElement("li")
+
+        li.setAttribute("data-id", pensamento.id)
+        li.classList.add("li-pensamento")
+
+        const iconeAspas = document.createElement("img")
+
+        iconeAspas.src = "assets/imagens/aspas-azuis.png"
+        iconeAspas.alt = "Aspes azuis"
+        iconeAspas.classList.add("icone-aspas")
+
+        const divConteudo = document.createElement("div")
+        
+        divConteudo.classList.add("pensamento-conteudo")
+        divConteudo.textContent = pensamento.conteudo
+
+        const divAutoria = document.createElement("div")
+        divAutoria.classList.add("pensamento-autoria")
+        divAutoria.textContent = pensamento.autoria    
+        
+        li.appendChild(iconeAspas)
+        li.appendChild(divConteudo)
+        li.appendChild(divAutoria)
+
+        listaPensamentos.appendChild(li)
+    } 
 }
 
 // Permite que o objeto seja acessado por outros arquivos
